a8af84d30a92d93525580e66384f8f96693b8220
Karen Herrera <837@holbertonschool.com>
2020-01-03 18:18:01 +0000

    number 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #sets up your web servers for the deployment
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu /data/
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+chown -R ubuntu:ubuntu /data/
 echo "Testinf Nginx Config" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-sudo nginx -t
-sudo service nginx restart
+nginx -t
+service nginx restart
 exit 0
